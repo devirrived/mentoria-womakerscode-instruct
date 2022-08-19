fae477502607554357cd99725151aac93cb9f57b
devirrived <devirrived@outlook.com>
2022-08-19 13:01:24 -0300

    Adiciona exemplo de Setter, Getter @property

---
+++ Getter_Setter_property/main.py
@@ -0,0 +1,40 @@
+# Getter [@property] = retornar o valor
+# Setter = definir ou atualizar
+
+class Quadrado:
+  def __init__(self, medida):
+    self.altura = medida
+    self.largura = medida
+
+  @property
+  def altura(self):
+    print('Getter de altura')
+    return self.__medida
+
+  @altura.setter
+  def altura(self, valor):
+    print('Setter de altura')
+
+    if valor < 0:
+      raise ValueError()
+    self.__medida = valor
+
+  @property
+  def largura(self):
+    print('Getter de largura')
+    return self.__medida
+
+  @largura.setter
+  def largura(self, valor):
+    print('Setter de largura')
+    if valor < 0:
+      raise ValueError()
+    self.__medida = valor
+
+  def area(self):
+    return self.largura * self.altura
+
+quadrado = Quadrado(2)
+quadrado.altura = 3
+quadrado.largura = 2
+print(quadrado.area())
